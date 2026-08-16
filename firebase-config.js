e9307023af51c38e8960b9613ace3eeb89edb1f5
harshit4088-lgtm <harshit4088@gmail.com>
2026-08-16 22:58:19 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGd7AS81Q53meBWVADmzDXAv_hwZaMMtw",
+  authDomain: "attendance-app-f5c57.firebaseapp.com",
+  databaseURL: "https://attendance-app-f5c57-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "attendance-app-f5c57"
+};
 
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.database();
