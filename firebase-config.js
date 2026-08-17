379efef3e0e9892b3d81ca91196e4e0c9a99d617
harshit4088-lgtm <harshit4088@gmail.com>
2026-08-17 22:28:34 +0530

    Update Firebase configuration for new project

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDGd7AS81Q53meBWVADmzDXAv_hwZaMMtw",
-  authDomain: "attendance-app-f5c57.firebaseapp.com",
-  databaseURL: "https://attendance-app-f5c57-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "attendance-app-f5c57"
+  apiKey: "AIzaSyDXmGKi5MqK4FMUtUXshRQeSm6ligqpQbo",
+  authDomain: "attendance-system-v2-69b9b.firebaseapp.com",
+  databaseURL: "https://attendance-system-v2-69b9b-default-rtdb.firebaseio.com",
+  projectId: "attendance-system-v2-69b9b",
+  storageBucket: "attendance-system-v2-69b9b.firebasestorage.app",
+  messagingSenderId: "932347549525",
+  appId: "1:932347549525:web:d2009a4e49f97ed581c1dc"
 };
 
 firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.database();
